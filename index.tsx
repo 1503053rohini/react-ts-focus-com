bd56e98cff98f36ea8ee3e71f379e6e4079d2140
1503053rohini <jadhavrohini199@gmail.com>
2019-11-28 10:57:56 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -17,3 +17,22 @@ let objarray = [1,3,4,6,7];
 let onjarray2 = [...objarray, 9, 10];
 console.log(objarray);
 console.log(onjarray2);
+
+let obj2 ={fname:'rohini', lname:'jadhav', salary: 4000};
+let obj3 = Object.assign({}, obj2, {salary:4567});
+console.log(obj2);
+console.log(obj3);
+
+//destructuring objects
+let obj4 ={fname:'rohini', lname:'jadhav', salary: 4000, add:'kapil'};
+let {fname, add, ...rest} = obj4;
+console.log(fname);
+console.log(add);
+console.log(rest);
+
+
+
+
+
+
+
