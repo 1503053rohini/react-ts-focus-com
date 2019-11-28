90f6cd7a627e570d26caa987af02ee1acf0cb9bc
1503053rohini <jadhavrohini199@gmail.com>
2019-11-28 10:41:26 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
 
-ReactDom.render(
- <App/>
-  , document.getElementById('root'));
+let object =
+{
+  firstname: 'rohini',
+  lastname : 'jadhav',
+  salary: 200
+};
+let object2={...object, salary:400};
+console.log(object2);
+
+
+//spread object ...oldobject
+let objarray = [1,3,4,6,7];
+let onjarray2 = [...objarray, 9, 10];
+console.log(objarray);
+console.log(onjarray2);
